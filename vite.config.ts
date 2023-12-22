80278982dc5ab5d27d21fd4ed98440a91df01610
Ramil Muratov <ramil@muratov.space>
2023-12-22 08:31:21 +0100

    canonical url, sitemap, robots.txt

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     sourcemap: true,
   },
-  plugins: [react()],
+  plugins: [react(), Sitemap({ hostname: 'https://sourcemap.tools' })],
 
   test: {
     // you might want to disable it, if you don't have tests that rely on CSS

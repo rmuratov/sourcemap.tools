4e70c760a9e6196380eaebd2763f485140b20fa8
Ramil Muratov <ramil@muratov.space>
2025-09-23 21:59:19 +0200

    Coverage report

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,5 @@
-/* v8 ignore start */
 module.exports = {
   content: ['./src/**/*.{ts,tsx}', 'index.html'],
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   darkMode: 'class',
 }
-/* v8 ignore end */
